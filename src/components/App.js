a58e63286932c82b1ce6dea95ea29764c6af23cb
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-27 00:02:35 +0300

    Implemented routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import About from './About';
+import Home from './Home';
+import Services from './Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/about" element={<About />}/>
+          <Route excat path="/services" element={<Services />}/>          
+        </Routes>
+      </Router>
+      
+    
     </div>
   );
 }

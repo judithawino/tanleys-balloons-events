6c3beccb80b5cf7795224cafccc4d2de5ad61638
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-27 00:03:44 +0300

    Created Home component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home(){
+
+    return(
+        <div>
+            <p>Welcome to Tanleys's balloons and events. We are number on whe it comes to event decoration. We are based in Kisumu but we come to you our client wherever you are.</p>
+        </div>
+    )
+}

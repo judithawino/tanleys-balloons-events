6b0e004fd77cc08491c0ed4047ab207247b5941c
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-26 23:37:48 +0300

    Added a h1 tag and center tag

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About(){
+
+    return(
+        <div>
+            <h1>About Us</h1>
+            <center>From set up to clean up, we bring your dreams to life while you have the fun. Whether it is a gender reveal, baby shower, bridal shower, graduation, engagement, private party, corporate launch, e.t.c. Talk to us and we will create an experiance of a lifetime. </center>
+        </div>
+    )
+}

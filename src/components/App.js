f72d4211e460a96c32f9458c5844ad1811d3d33d
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-27 22:49:53 +0300

    added the image logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
+import logo_design from './logo_design.png'
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import About from './About';
 import Home from './Home';
 import Services from './Services';
+// import reactjsx from "./reactjsx.png";
+
 
 function App() {
   return (
@@ -13,11 +16,11 @@ function App() {
         
         <div className="navbar bg-light">
           <h1 className="navbar-brand">
-            <img src="" alt="Company Logo" class="d-inline-block align-text-top"></img> 
+            <Link to="/logo_design"><img id="App-logo" src={logo_design} alt="Company Logo" className="d-inline-block align-text-top"></img></Link>
             Tanleys' Balloons and Events</h1>          
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-          <Link className="nav-link" to="/about">About</Link>
-          <Link className="nav-link" to="/services">Services</Link>                   
+          <Link className="nav-link active" to="/about">About</Link>
+          <Link className="nav-link active" to="/services">Services</Link>                   
         </div>
         <Routes>
           <Route exact path="/" element={<Home />}/>

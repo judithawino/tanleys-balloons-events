bbca7ecd6dd0ffb13660e8ae8ffa822332e0177b
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-28 15:55:55 +0300

    Modified footer text color and nav background

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       
       <Router>        
-        <div className="navbar bg-light">
-          <h1 className="navbar-brand">
+        <div className="navbar" style={{background:'#27104e', color:'#FFFFFF'}}>
+          <h1 className="navbar-brand" style={{color:'#FFFFFF'}}>
             <Link to="/logo_design"><img id="App-logo" src={logo_design} alt="Company Logo" className="d-inline-block align-text-top"></img></Link>
             Tanleys' Balloons and Events</h1>          
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
@@ -31,19 +31,19 @@ function App() {
 
         <div>
         <div className="container" style={{background:'#27104e', maxWidth:"100%"}}>
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style={{width:'100%'}}>
-          <p className="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style={{width:'100%', color:'#FFFFFF'}}>
+          <p className="col-md-4 mb-0 "style={{color:'#FFFFFF'}}>© 2022 Company, Inc</p>
 
           <Link to="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
             
           </Link>
 
           <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">Home</Link></li>
-            <li className="nav-item"><Link to="/about" className="nav-link px-2 text-muted">About</Link></li>
-            <li className="nav-item"><Link to="/services" className="nav-link px-2 text-muted">Services</Link></li>
-            <li className="nav-item"><Link to="/faqs" className="nav-link px-2 text-muted">FAQs</Link></li>
-            <li className="nav-item"><Link to="contact" className="nav-link px-2 text-muted">Contact</Link></li>
+            <li className="nav-item"><Link to="/" className="nav-link px-2" style={{color:'#FFFFFF'}}>Home</Link></li>
+            <li className="nav-item"><Link to="/about" className="nav-link px-2 " style={{color:'#FFFFFF'}}>About</Link></li>
+            <li className="nav-item"><Link to="/services" className="nav-link px-2" style={{color:'#FFFFFF'}}>Services</Link></li>
+            <li className="nav-item"><Link to="/faqs" className="nav-link px-2 " style={{color:'#FFFFFF'}}>FAQs</Link></li>
+            <li className="nav-item"><Link to="contact" className="nav-link px-2 " style={{color:'#FFFFFF'}}>Contact</Link></li>
           </ul>
         </footer>
       </div>

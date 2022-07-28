adbc5bf8fb3935b4082899574fcea524fff8eb2b
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-28 18:53:07 +0300

    Imported useState,useEffect and created handleServiceFetch()

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export default function Services(){
+    const[service,setService] = useState([]);
+    const{id, title, description, price, picture}= serviceObje
+
+    function handleServiceFetch(){
+        fetch('https://tanleys.herokuapp.com/services')
+        .then((resp)=>resp.json())
+        .then((data)=>{
+            setService(data)
+        
+        })
+    }
+    useEffect(handleServiceFetch,[])
     return (
         <div>
             <h1> Services</h1>
             <p>Our services include ...</p>
+            <div key={id}>
+                {service}
+            </div>
         </div>
     )
 }

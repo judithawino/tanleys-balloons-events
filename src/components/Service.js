41a917da6569bf3bb2fb00d3766188b0e6d7c532
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-29 07:24:30 +0300

    Introduced cards

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 export default function Service({serviceObje}){
     const{id, title, description, price, picture}= serviceObje
+
     return(
-        
-            <div>
-              <h3>{title}</h3>
-              <p>{description}</p>
-              <img src={picture} alt="Picture illustartion" />
-              <p>
-              Price Ranges from <strong> {price}</strong> and above
-              </p>
-            </div>  
+        <div className="card mb-3" style={{maxwidth: "540px"}}>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={picture} alt="Picture illustartion" class="img-fluid rounded-start" />
+                </div>
+                <div className="col-md-8">
+                <div className="card-body">
+                    <h3 className="card-title">{title}</h3>
+                    <p className="card-text">{description}</p>
+                    <p className='lead'>
+                    Price Ranges from <strong> {price}</strong> and above
+                    </p>
+                </div>
+                </div> 
+            </div>       
+        </div>  
 
     )
 }

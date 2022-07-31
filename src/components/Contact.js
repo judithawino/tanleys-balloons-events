9bcc96749c63db2f8a53e8ab95ab9b671bd80cdd
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-31 21:36:52 +0300

    Modified alt and removed unused variables

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact(){
+    return(
+        <div className="contact" style={{width: 360 + 'rem'}}>
+        {/* <div className='card' style={{width: 18 + 'rem'}}> */}
+        <div >
+                <h1>Contact</h1>
+                <p>Tel:+2541 1259 8446</p>
+                <p>WhatsApp:+2541 1259 8446</p>
+                <p>Email:tanleysevents@gmail.com</p>
+        </div>
+        </div> 
+        // </div>
+    )
+}
